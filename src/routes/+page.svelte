c55f2ee2bf2646dcda6e308f336a27024190e6cb
NotCreative21 <seanray410@gmail.com>
2022-11-19 22:18:06 -0500

    basic site layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Caffeinated - Home</title>
+	<meta name="Caffeinated Robotics" content="Home page" />
+</svelte:head>
+
+<section>
+    <div class="flex justify-center text-center">
+        <div class="text-5xl">
+            Our History
+        </div>
+    </div>
+    <div class="flex justify-center text-center">
+        <div class="max-w-7xl">
+            <div class="flex flex-wrap justify-center text-center content-center">
+                <img class="h-96 m-4 rounded-md" src="http://1.bp.blogspot.com/-m-xzL7vJlnc/UTyHAhrsnUI/AAAAAAAAJP8/V0HDXJBbtFo/s1600/monkey+growl.jpg" alt="" />
+                <div class="text-xl m-4 max-w-4xl content-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center text-center">
+        <div class="text-5xl">
+            FIRST Robotics
+        </div>
+    </div>
+    <div class="flex justify-center text-center">
+        <div class="max-w-7xl">
+            <div class="flex flex-wrap justify-center text-center content-center">
+                <div class="text-xl m-4 max-w-4xl content-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+                <img class="h-96 m-4 rounded-md" src="http://1.bp.blogspot.com/-m-xzL7vJlnc/UTyHAhrsnUI/AAAAAAAAJP8/V0HDXJBbtFo/s1600/monkey+growl.jpg" alt="" />
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center text-center">
+        <div class="text-5xl">
+            Team Profile
+        </div>
+    </div>
+    <div class="flex justify-center text-center">
+        <div class="max-w-7xl">
+            <div class="flex flex-wrap justify-center text-center content-center">
+                <img class="h-96 m-4 rounded-md" src="http://1.bp.blogspot.com/-m-xzL7vJlnc/UTyHAhrsnUI/AAAAAAAAJP8/V0HDXJBbtFo/s1600/monkey+growl.jpg" alt="" />
+                <div class="text-xl m-4 max-w-4xl content-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { page } from '$app/stores';
+    import logo from "$lib/images/logo.png";
+	import github from '$lib/images/github.svg';
+</script>
+
+<header class="bg-[#f4eae1]">
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="logo" />
+		</a>
+	</div>
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/robot">Robot</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/blog">Blog</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/sponsors">Sponsors</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header>
+
+<style>
+</style>
