cdc4907e5db0d675be91c8ccef7b6ef847daf5a6
notseanray <seanray410@gmail.com>
2023-01-27 20:06:20 -0500

    correct descriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,9 +73,9 @@
 					</div>
 				</div>
 			</div>
-            <div class="flex justify-center text-center -translate-y-28">
-                <img class="mobile-logo m-1" src={logo} alt="" />
-            </div>
+			<div class="flex justify-center text-center -translate-y-28">
+				<img class="mobile-logo m-1" src={logo} alt="" />
+			</div>
 		</div>
 		<div class="mobile-title">
 			<div class="mobile-name text-4xl font-extrabold">caffeinated</div>
@@ -84,137 +84,137 @@
 		</div>
 	</div>
 	<div class="mt-60" />
-    <div class="mx-[1%]">
-        <div class="flex flex-wrap justify-center">
-            <div class="flex justify-center text-center mr-2">
-                <div>
-                    <div class="mobile-name uppercase font-extrabold flex ml-2">
-                        first
-                        {#if !mobile}
-                            <img class="ml-16 h-36 m-2 rounded-md" src={first_logo} alt="" />
-                        {/if}
-                    </div>
-                    <div class="flex -translate-y-4">
-                        <div class="dot3" />
-                        <div class="dot3" />
-                        <div class="dot3" />
-                        <div class="dot3" />
-                        <div class="dot3" />
-                        <div class="dot3" />
-                        <div class="dot3" />
-                        <div class="dot3" />
-                    </div>
-                    <div>
-                        <div class="text-boxes m-2 max-w-[45vw] content-center font-bold text-left mt-8">
-                            In 1989, Dean Kamen started the nonprofit organization FIRST, which stands for "For the
-                            Inspiration and Recognition of Science and Technology". The FIRST Robotics Competition
-                            is a high-school robotics competition that combines gracious professionalism with
-                            technology to create an environment that encourages a better appreciation for STEM.
-                            Professional mentors and college students collaborate with teams to educate students in
-                            essential engineering concepts and professional skills while also assisting the team in
-                            its success.
-                        </div>
-                    </div>
-                </div>
-            </div>
+	<div class="mx-[1%]">
+		<div class="flex flex-wrap justify-center">
+			<div class="flex justify-center text-center mr-2">
+				<div>
+					<div class="mobile-name uppercase font-extrabold flex ml-2">
+						first
+						{#if !mobile}
+							<img class="ml-16 h-36 m-2 rounded-md" src={first_logo} alt="" />
+						{/if}
+					</div>
+					<div class="flex -translate-y-4">
+						<div class="dot3" />
+						<div class="dot3" />
+						<div class="dot3" />
+						<div class="dot3" />
+						<div class="dot3" />
+						<div class="dot3" />
+						<div class="dot3" />
+						<div class="dot3" />
+					</div>
+					<div>
+						<div class="text-boxes m-2 max-w-[45vw] content-center font-bold text-left mt-8">
+							The FIRST Robotics Competition is an international high-school competitive robots
+							competition centered around industry-scale robots. Each year a new challenge is
+							released on January 7, and teams have 7 weeks to design and build their robots before
+							competing with them. Our team is located in the Peachtree District (encompassing
+							Georgia and South Carolina). We will be competing in the Gwinnett and Carrollton PCH
+							District Qualifiers on 3/9-3/11 and 3/23-3/25 respectively, with the goal of advancing
+							to PCH District Champs and FIRST Champs.
+						</div>
+					</div>
+				</div>
+			</div>
 
-            <div class="flex justify-center mt-4 text-right float-right ml-2">
-                <div class="flex flex-wrap justify-center text-right float-right">
-                    <div class="text-boxes m-2 max-w-[45vw] content-center font-bold text-right">
-                        "FIRST creates programs where its participants are limited only by the bounds of their
-                        imagination and gives them a forum to begin to examine the world’s problems by exploring
-                        science, technology, engineering and math.
-                        <br />
-                        - Congressman Jim Langevin (RI)
-                        {#if !mobile}
-                            <iframe
-                                class="ml-6 rounded-md float-right mt-10"
-                                width="560"
-                                height="315"
-                                src="https://www.youtube.com/embed/Jd29kzjclV0"
-                                title="YouTube video player"
-                                frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen
-                            />
-                        {:else}
-                            <div class="flex flex-center justify-center">
-                                <iframe
-                                    class="ml-6 rounded-md float-right mt-10"
-                                    width="250"
-                                    height="141"
-                                    src="https://www.youtube.com/embed/Jd29kzjclV0"
-                                    title="YouTube video player"
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen
-                                />
-                            </div>
-                        {/if}
-                        <br />
-                        <div class="flex first-dots-ending float-right">
-                            <div class="dot3" />
-                            <div class="dot3" />
-                            <div class="dot3" />
-                            <div class="dot3" />
-                            <div class="dot3" />
-                            <div class="dot3" />
-                            <div class="dot3" />
-                            <div class="dot3" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mt-80" />
-        <div class="flex flex-wrap">
-            <div class="flex justify-center text-center">
-                <div>
-                    <div class="mobile-name font-extrabold flex ml-1">the team</div>
-                    <div>
-                        <div class="text-boxes m-2 max-w-[80vw] content-center font-bold text-left mt-8">
-                            Caffeinated is a community-based FIRST Robotics Competition team in Marietta, GA. The
-                            team was founded on November, 2022 with the mission of not only to be a competitive FRC
-                            team, but also to provide a productive, engaging environment for high schoolers in the
-                            area to develop advanced skills in engineering and project management. With experienced
-                            mentors and coaches around to provide a guiding hand, we are able to organically learn
-                            how to come to their own conclusions through their own research and analysis. This
-                            allows us a deeper takeaway than simply being given the solution.
-                        </div>
-                    </div>
-                    <div class="flex mt-8">
-                        {#if !mobile}
-                            <div class="bottom-0 left-4 translate-y-24">
-                                <div class="dot2" />
-                                <div class="dot2" />
-                                <div class="dot2" />
-                                <div class="dot2" />
-                            </div>
-                        {/if}
-                        <div class="flex flex-wrap max-w-full team-profile">
-                            <img class="mx-4 my-4 h-60 rounded-md" src={ian_oberbeck} alt="" />
-                            <img class="mx-4 my-4 h-60 rounded-md" src={jacob_dymkowski} alt="" />
-                            <img class="mx-4 my-4 h-60 rounded-md" src={leona_longhurst} alt="" />
-                            <img class="mx-4 my-4 h-60 rounded-md" src={sameeh_pottayil} alt="" />
-                            <img class="mx-4 my-4 h-60 rounded-md" src={sean_ray} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {#if !mobile}
-                <div class="bottom-0 right-5">
-                    <div class="dot2" />
-                    <div class="dot2" />
-                    <div class="dot2" />
-                    <div class="dot2" />
-                    <div class="dot2" />
-                    <div class="dot2" />
-                    <div class="dot2" />
-                    <div class="dot2" />
-                </div>
-            {/if}
-        </div>
-    </div>
+			<div class="flex justify-center mt-4 text-right float-right ml-2">
+				<div class="flex flex-wrap justify-center text-right float-right">
+					<div class="text-boxes m-2 max-w-[45vw] content-center font-bold text-right">
+						"FIRST creates programs where its participants are limited only by the bounds of their
+						imagination and gives them a forum to begin to examine the world’s problems by exploring
+						science, technology, engineering and math.
+						<br />
+						- Congressman Jim Langevin (RI)
+						{#if !mobile}
+							<iframe
+								class="ml-6 rounded-md float-right mt-10"
+								width="560"
+								height="315"
+								src="https://www.youtube.com/embed/Jd29kzjclV0"
+								title="YouTube video player"
+								frameborder="0"
+								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+								allowfullscreen
+							/>
+						{:else}
+							<div class="flex flex-center justify-center">
+								<iframe
+									class="ml-6 rounded-md float-right mt-10"
+									width="250"
+									height="141"
+									src="https://www.youtube.com/embed/Jd29kzjclV0"
+									title="YouTube video player"
+									frameborder="0"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+									allowfullscreen
+								/>
+							</div>
+						{/if}
+						<br />
+						<div class="flex first-dots-ending float-right">
+							<div class="dot3" />
+							<div class="dot3" />
+							<div class="dot3" />
+							<div class="dot3" />
+							<div class="dot3" />
+							<div class="dot3" />
+							<div class="dot3" />
+							<div class="dot3" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mt-80" />
+		<div class="flex flex-wrap">
+			<div class="flex justify-center text-center">
+				<div>
+					<div class="mobile-name font-extrabold flex ml-1">the team</div>
+					<div>
+						<div class="text-boxes m-2 max-w-[80vw] content-center font-bold text-left mt-8">
+							Caffeinated is a community-based FIRST Robotics Competition team (#9293) based in
+							Marietta, GA. The team was founded in November of 2022 by a group of highly motivated
+							students with the mission to provide a productive, enriching environment for local
+							high-schoolers to practically apply engineering. The team is made possible by the
+							generous support of our corporate sponsors and the industry experts who volunteer
+							their time to mentor us. Caffeinated emphasizes process and analysis-backed decision
+							making when designing our robots, creating intentional designs; nothing is arbitrary.
+						</div>
+					</div>
+					<div class="flex mt-8">
+						{#if !mobile}
+							<div class="bottom-0 left-4 translate-y-24">
+								<div class="dot2" />
+								<div class="dot2" />
+								<div class="dot2" />
+								<div class="dot2" />
+							</div>
+						{/if}
+						<div class="flex flex-wrap max-w-full team-profile">
+							<img class="mx-4 my-4 h-60 rounded-md" src={ian_oberbeck} alt="" />
+							<img class="mx-4 my-4 h-60 rounded-md" src={jacob_dymkowski} alt="" />
+							<img class="mx-4 my-4 h-60 rounded-md" src={leona_longhurst} alt="" />
+							<img class="mx-4 my-4 h-60 rounded-md" src={sameeh_pottayil} alt="" />
+							<img class="mx-4 my-4 h-60 rounded-md" src={sean_ray} alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+			{#if !mobile}
+				<div class="bottom-0 right-5">
+					<div class="dot2" />
+					<div class="dot2" />
+					<div class="dot2" />
+					<div class="dot2" />
+					<div class="dot2" />
+					<div class="dot2" />
+					<div class="dot2" />
+					<div class="dot2" />
+				</div>
+			{/if}
+		</div>
+	</div>
 </section>
 
 <style>
@@ -260,7 +260,7 @@
 		width: min(36rem, 60vh);
 	}
 	.mobile-title {
-        transform: translateY(-22rem);
+		transform: translateY(-22rem);
 		margin-top: 30rem;
 	}
 	.mobile-name {
@@ -272,67 +272,68 @@
 	.mobile-steam {
 		margin-left: min(10rem, 12vh);
 	}
-    .first-dots-ending {
-        width: 20rem;
-    }
+	.first-dots-ending {
+		width: 20rem;
+	}
 	.dot {
 		animation-name: wave;
 		animation-iteration-count: infinite;
 		animation-duration: 5s;
 		animation-timing-function: linear;
-		height: .8rem;
-		width: .8rem;
+		height: 0.8rem;
+		width: 0.8rem;
 		border: 1px solid #ffe9d3;
 		background: #ffe9d3;
 		z-index: 2;
 		border-radius: 50%;
 		margin: 1.1rem;
 	}
-    .steam-grid-translate1 {
-        margin-top: 3.8rem;
-    }
-    .steam-grid-translate2 {
-        margin-top: 9.5rem;
-    }
-    .team-profile {}
-    .text-boxes {
-        font-size: 1.875rem;
-        line-height: 2.25rem;
-    }
-    .logo-translate {
-        transform: translateY(-10vh);
-    }
+	.steam-grid-translate1 {
+		margin-top: 3.8rem;
+	}
+	.steam-grid-translate2 {
+		margin-top: 9.5rem;
+	}
+	.team-profile {
+	}
+	.text-boxes {
+		font-size: 1.875rem;
+		line-height: 2.25rem;
+	}
+	.logo-translate {
+		transform: translateY(-10vh);
+	}
 	@media (max-width: 1200px) {
-        .text-boxes {
-            font-size: 1rem;
-            line-height: 1.5rem;
-        }
-        .team-profile {
-            display: flex;
-            justify-content: center;
-        }
-        .first-dots-ending {
-            width: 90vw;
-        }
+		.text-boxes {
+			font-size: 1rem;
+			line-height: 1.5rem;
+		}
+		.team-profile {
+			display: flex;
+			justify-content: center;
+		}
+		.first-dots-ending {
+			width: 90vw;
+		}
 		.mobile-logo {
 			height: 90vw;
 			width: 90vw;
-            max-width: 500px;
-            max-height: 500px;
-            margin-top: 1rem;
-            margin-left: 10vw;
+			max-width: 500px;
+			max-height: 500px;
+			margin-top: 1rem;
+			margin-left: 10vw;
 		}
 		.mobile-steam {
 			margin-left: 20vw;
-            margin-top: 5vh;
+			margin-top: 5vh;
 		}
-        .logo-translate {
-            transform: translateY(2vh);
-        }
+		.logo-translate {
+			transform: translateY(2vh);
+		}
 		.mobile-title {
 			margin-top: 1rem;
 			margin-left: 10vw;
-            transform: translateY(max(-25vw, -20vh));
+			transform: translateY(max(-25vw, -20vh));
 		}
 		.mobile-name {
 			font-size: 8vw;
